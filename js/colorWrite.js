c003e1002af8d5bc70bb1a1adbac011a8dbb6262
INVERTER_TEAM <invertersup8ort11@gmail.com>
2026-03-14 15:05:36 +0200

    Big Project Update!

--- js/colorWrite.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("saveColor").addEventListener("click", () => {
-    const pickedColor = document.getElementById("colorPicker").value;
-    window.electronAPI.saveColor(pickedColor);
-});
